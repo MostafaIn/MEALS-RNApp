37778d403338eda55936293f98ffcae2b87161a0
mostafaIn <mostafa2015se@gmail.com>
2020-03-27 12:13:13 +0200

    132. MaterialBottomTabNavigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,6 +1,7 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
 import React from 'react'
 import { Platform } from 'react-native'
@@ -33,24 +34,36 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-const MealsFavTabNavigator = createBottomTabNavigator({
+
+const tabScreenConfig = {
     Meals:{
         screen:MealsNavigator,
         navigationOptions:{
           tabBarIcon: (tabInfo) =>{
               return <MaterialIcons name="restaurant" size={25} color={tabInfo.tintColor} />
-          }  
-        }
+          },
+          tabBarColor: Colors.secondary  
+        },
     },
     Favorites:{
         screen: FavoritesScreen,
         navigationOptions:{
             tabBarIcon: (tabInfo) =>{
                 return <MaterialIcons name="stars" size={25} color={tabInfo.tintColor} />
-            }
-        }
+            },
+            tabBarColor: Colors.primary
+        },
     }
-},{
+}
+
+const MealsFavTabNavigator = Platform.OS === 'android'
+    ?
+    createMaterialBottomTabNavigator(tabScreenConfig,{
+        activeColor: '#12f',
+        shifting: true
+    })
+    :
+    createBottomTabNavigator(tabScreenConfig,{
     tabBarOptions:{
         activeTintColor: '#12f'
     }
