2e58cf64219c0550673acf05dffb491ec255b970
mostafaIn <mostafa2015se@gmail.com>
2020-03-30 15:16:03 +0300

    151. Rendering a Fallback Text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,16 +1,28 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
+import { AntDesign } from '@expo/vector-icons'
+
 import { useSelector } from 'react-redux';
 
 import MealList from '../components/MealList';
 
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 import HeaderButton from '../components/HeaderButton'
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = (props) => {
     const favMeals = useSelector(state => state.meals.favoriteMeals);
-    
+
+    if(!favMeals || favMeals >= 0){
+        return <View style={styles.content}>
+            <AntDesign name="antdesign" size={100} color="#12f" />
+            <View style={{alignItems:'center'}}>
+                <DefaultText>NOT FOUND</DefaultText>
+                <DefaultText>start adding some!</DefaultText>
+            </View>
+        </View>
+    }
     return <MealList listData={favMeals} navigation={props.navigation} />
 };
 
@@ -29,4 +41,10 @@ const FavoritesScreen = (props) => {
 
 export default FavoritesScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    content:{
+        flex:1,
+        justifyContent:'space-around',
+        alignItems:'center'
+    }
+})
